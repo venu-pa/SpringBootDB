773601918a2bdb186fc561d676d2e7321a1b8489
venu-pa <venuparuchuri4856@gmail.com>
2022-08-25 07:08:33 +0530

    base project

---
+++ DBOperations/src/main/java/com/example/DBOperations/DbOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.DBOperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbOperationsApplication.class, args);
+	}
+
+}
---
+++ DBOperations/src/test/java/com/example/DBOperations/DbOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DBOperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
